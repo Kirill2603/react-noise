b0ab986fee614d81a8b77b02217c7146419cec79
kirill <k.radobolsky@gmail.com>
2022-10-12 14:32:27 +0300

    add svg buttons

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,13 +1,15 @@
 import { FC, useState } from 'react'
+import { ReactComponent as PlaySvg } from 'assets/circle-play.svg'
+import { ReactComponent as PauseSvg } from 'assets/circle-pause.svg'
 
 type PlayerProps = {
-	playableAudio: {audio: string, title: string}
+	playableAudio: { audio: string, title: string }
 }
 
 export const Player: FC<PlayerProps> = ({ playableAudio }) => {
 
 	const [play, setPlay] = useState(false)
-	const [audio] = useState(new Audio(playableAudio.audio));
+	const [audio] = useState(new Audio(playableAudio.audio))
 	audio.loop = true
 
 	const onSetPlay = () => {
@@ -23,7 +25,13 @@ export const Player: FC<PlayerProps> = ({ playableAudio }) => {
 		<>
 			<div>
 				<h3>{playableAudio.title}</h3>
-				<button onClick={onSetPlay}>{play ? '||' : '>'}</button>
+				<button onClick={onSetPlay}>
+					{play
+						?
+						<PauseSvg className='w-10 h-10' />
+						:
+						<PlaySvg className='w-10 h-10' />}
+				</button>
 			</div>
 		</>
 	)
